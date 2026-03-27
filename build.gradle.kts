6eb5b3d04855f38cf08e4e7dccfd316dc9e65a7a
Gyro Skalitz <gyroskalitz@outlook.com>
2026-03-27 17:43:34 +0800

    添加pom信息

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,20 @@ publishing {
             groupId = project.group.toString()
             artifactId = "adhe-re-quest-core"
             version = project.version.toString()
+            pom {
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("gyroskalitz")
+                        name.set("Gyro Skalitz")
+                    }
+                }
+            }
         }
     }
 
